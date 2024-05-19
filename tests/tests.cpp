bb8779032a3848dd99828580a27a2136856e85b8
xisseu <164639507+xisseu@users.noreply.github.com>
2024-05-19 23:42:39 +0300

    Update tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,11 +1,19 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "IProductA.h"
+#include "IProductB.h"
 #include "IFactory.h"
 
 class MockFactory : public IFactory {
 public:
     MOCK_METHOD(IProductA*, createConcreteProductA, (), (const override));
     MOCK_METHOD(IProductB*, createConcreteProductB, (), (const override));
+};
+
+class MockProductA : public IProductA {
+public:
+    MOCK_METHOD(void, init, (), (const override));
+    MOCK_METHOD(void, performTask, (), (const override));
 };
 
 TEST(IFactoryTest, CreateConcreteProductA) {
@@ -21,20 +29,19 @@ TEST(IFactoryTest, CreateConcreteProductA) {
 TEST(IFactoryTest, CreateConcreteProductB) {
     auto mockFactory = std::make_shared<MockFactory>();
 
-    EXPECT_CALL(*mockFactory, createConcreteProductB()).Times(1).WillOnce(Return(nullptr));
+    EXPECT_CALL(*mockFactory, createConcreteProductB()).WillOnce(Return(nullptr));
 
     IProductB* productB = mockFactory->createConcreteProductB();
 
     ASSERT_EQ(productB, nullptr);
 }
 
-TEST(IFactoryTest, CreateAllProducts) {
-    auto mockFactory = std::make_shared<MockFactory>();
+TEST(IProductATest, InitAndPerformTask) {
+    auto mockProductA = std::make_shared<MockProductA>();
 
-    EXPECT_CALL(*mockFactory, createConcreteProductA()).Times(1);
-    EXPECT_CALL(*mockFactory, createConcreteProductB()).Times(1);
+    EXPECT_CALL(*mockProductA, init()).Times(1);
+    EXPECT_CALL(*mockProductA, performTask()).Times(1);
 
-    mockFactory->createConcreteProductA();
-    mockFactory->createConcreteProductB();
+    mockProductA->init();
+    mockProductA->performTask();
 }
-
